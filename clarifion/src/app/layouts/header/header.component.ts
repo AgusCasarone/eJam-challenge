a0bc353d65ca39cd5b9535b911730a41c49190ee
Agustina Casarone <agus.casarone@gmail.com>
2024-06-29 16:05:00 -0300

    nav and header style

--- clarifion/src/app/layouts/header/header.component.ts
+++ clarifion/src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,6 +11,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  isSmallScreen: boolean = false;
+
+  constructor(
+    @Inject(PLATFORM_ID)
+    private platformId: Object
+  ){}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth < 768;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth < 768;
+    }
+  }
 
 }
