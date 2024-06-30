0d72a6e955503e9f25dd10ef6a79dcdebfe29ebd
Agustina Casarone <agus.casarone@gmail.com>
2024-06-30 08:09:44 -0300

    user-review

--- clarifion/src/app/views/customer-review/customer-review.component.ts
+++ clarifion/src/app/views/customer-review/customer-review.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { UserIdentityComponent } from "./user-identity/user-identity.component";
+import { UserReviewComponent } from "./user-review/user-review.component";
 
 @Component({
-  selector: 'customer-review',
-  standalone: true,
-  imports: [],
-  templateUrl: './customer-review.component.html',
-  styleUrl: './customer-review.component.scss'
+    selector: 'customer-review',
+    standalone: true,
+    templateUrl: './customer-review.component.html',
+    styleUrl: './customer-review.component.scss',
+    imports: [UserIdentityComponent, UserReviewComponent]
 })
 export class CustomerReviewComponent {
 
