003c7412d43ebcfd2c9b338f1b3e6cfec72aa25a
Agustina Casarone <agus.casarone@gmail.com>
2024-06-30 07:21:25 -0300

    payment info

--- clarifion/src/app/views/item-detail/item-pay/payment-info/payment-info.component.ts
+++ clarifion/src/app/views/item-detail/item-pay/payment-info/payment-info.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'payment-info',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './payment-info.component.html',
   styleUrl: './payment-info.component.scss'
 })
-export class PaymentInfoComponent {
+export class PaymentInfoComponent implements OnInit {
 
+  isSmallScreen: boolean = false;
+
+  constructor(
+    @Inject(PLATFORM_ID)
+    private platformId: Object
+  ){}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth < 768;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth < 768;
+    }
+  }
 }
